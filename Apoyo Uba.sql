be7a3efd80894801c9f1dd026c9b1236380646ea
PriAnto06 <priscilaantonella.d.07@gmail.com>
2025-09-02 16:52:08 -0300

    seguimos con el proyecto apoyo uba

--- Apoyo Uba.sql
+++ Apoyo Uba.sql
@@ -90,4 +90,69 @@ CREATE TABLE [DiseñoSoftware] (
 	[Profesor]	VARCHAR(60) NOT NULL,
 	PRIMARY KEY([DiseñoSoftware_id] AUTOINCREMENT)
 );
+--para ver hacer 
+SELECT * FROM Ingles
 
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Present Continuos','Gabriel Beronda')
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('On Christmas year','Gabriel Beronda')
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Going will future simple','Gabriel Beronda')
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Future:Going to','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Grammar Going to','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Grammar Be going to','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Presente Simple positve-negative','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Presente Simple fun activities','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Past simple','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Past Continuos irregular verbs','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Past Continuos irregular verbs and regular verbs','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('Past Continuos irregular verbs and regular verbs grammar','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('verb to be','Gabriel Beronda');
+INSERT INTO Ingles (Apuntes,Profesor) VALUES
+('past verb to be','Gabriel Beronda');
+
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Conceptos basicos de base de datos','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('sistema de gestion de base de datos','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Introduccion a sql historias y usos','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Sentencias ,CREATE,ALTER,DROP','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Tipos de datos en SQL','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Sentencias ,INSERT,SELECT,UPDATE,DELETE','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Consultas simples en SQL,SELECT,WHERE,ORDER BY,LIMIT','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Funciones de agregado en SQL,COUNT,SUM,AVG,MAX,MIN','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Agrupaciones y subconsultas en SQL,GROUP BY,HAVING','Tomas Mayorga');
+INSERT INTO AGBD (Apuntes,Profesor) VALUES
+('Joins en SQL,INNER JOIN,LEFT JOIN,RIGHT JOIN,FULL JOIN','Tomas Mayorga');
+
+INSERT INTO TAP (Apuntes,Profesor) VALUES
+('Introduccion a Algoritmos','Julian Impelluso');
+INSERT INTO TAP (Apuntes,Profesor) VALUES
+('Introduccion a TIC-80','Julian Impelluso');
+INSERT INTO TAP (Apuntes,Profesor) VALUES
+('Unidad 1','Julian Impelluso');
+INSERT INTO TAP (Apuntes,Profesor) VALUES
+('Unidad 2-Funciones','Julian Impelluso');
+
+INSERT INTO DiseñoSoftware (Apuntes,Profesor) VALUES
+('Introduccion a Git y Github','Andres Navarro');
+INSERT INTO DiseñoSoftware (Apuntes,Profesor) VALUES
+('Introduccion a Flask + ejercicios','Andres Navarro');
